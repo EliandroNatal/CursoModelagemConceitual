26cdb5419b04b3cb8bcda12cc4eeaaee10a75f02
Eliandro Silva <eliandrosilva22@gmail.com>
2020-06-21 21:02:34 -0300

    Projeto criado

---
+++ src/main/java/com/eliandro/javaaula/JavaaulaApplication.java
@@ -0,0 +1,13 @@
+package com.eliandro.javaaula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaaulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaaulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliandro/javaaula/JavaaulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliandro.javaaula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaaulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
